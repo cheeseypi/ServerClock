37773edcbfdfc88d21d07c2392bafe614a6500f5
Skye Doto <skye@doto.works>
2026-03-13 04:10:25 -0400

    Basic styling

--- app.js
+++ app.js
@@ -2,33 +2,34 @@ var fs = require("fs"),
     express = require('express'),
     app = express();
 
-app.get("/ServerDate.js", function(req, res){
-    fs.readFile('lib/ServerDate.js', 'utf8', function (err, data) {
-        var now = Date.now();
+let script = fs.readFileSync('lib/ServerDate.js', 'utf8');
 
-        if (err)
-            res.status(500);
-        else {
-            if (req.query.time) {
-                res.set("Content-Type", "application/json");
-                res.json(now);
-            }
-            else {
-                res.set("Content-Type", "text/javascript");
-                res.send(data + "(" + now + ");\n");
-            }
+app.get("/ServerDate.js", function (req, res) {
+    var now = Date.now();
+
+    if (!script)
+        res.status(500);
+    else {
+        if (req.query.time) {
+            res.set("Content-Type", "application/json");
+            res.json(now);
         }
-    });
+        else {
+            res.set("Content-Type", "text/javascript");
+            res.send(script + "(" + now + ");\n");
+        }
+    }
 });
 
-app.get('/qr-creator', function(req, res) {
-    res.sendFile('lib/qr-creator.min.js', {root: __dirname});
-})
+app.use(express.static('public'));
+// app.get('/qr-creator', function (req, res) {
+//     res.sendFile('lib/qr-creator.min.js', { root: __dirname });
+// })
 
-app.get("/", function(req, res){
-    res.sendFile("index.html", {root: __dirname});
-});
+// app.get("/", function (req, res) {
+//     res.sendFile("index.html", { root: __dirname });
+// });
 
 port = process.env.PORT || 3000;
 app.listen(port);
-console.log('Listening on port '+port);
+console.log('Listening on port ' + port);
